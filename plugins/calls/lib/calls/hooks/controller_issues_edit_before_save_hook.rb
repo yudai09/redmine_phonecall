533168299e3ef1128815dfdfbd34c95c8d8db2ba
volreijy <nakano.masatoshi@stf.nifty.co.jp>
2017-03-10 19:22:54 +0900

    delete unnecessary files

--- plugins/calls/lib/calls/hooks/controller_issues_edit_before_save_hook.rb
+++
@@ -1,7 +0,0 @@
-module Calls
-  class Hooks < Redmine::Hook::ViewListener
-    def controller_issues_edit_before_save(context={})
-      Rails.logger.info('controller_issues_edit_before_save calling!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    end
-  end
-end
