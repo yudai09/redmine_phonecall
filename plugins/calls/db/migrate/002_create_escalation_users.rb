6381f6fce2f3ea62d25498fc2f078382532be522
unknown <kato.yudai@nifty.co.jp>
2017-03-15 00:35:02 +0900

    remove wrong assumption about national prefix code.

--- plugins/calls/db/migrate/002_create_escalation_users.rb
+++ plugins/calls/db/migrate/002_create_escalation_users.rb
@@ -2,7 +2,7 @@ class CreateEscalationUsers < ActiveRecord::Migration
   def change
     create_table :escalation_users do |t|
       t.string :name
-      t.integer :phone_number, :limit => 11
+      t.integer :phone_number, :limit => 256
       t.integer :priority
     end
   end
