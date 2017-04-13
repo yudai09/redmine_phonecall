bb6f00612558b2cb6894b87dea2352a06221fed1
volreijy <nakano.masatoshi@stf.nifty.co.jp>
2017-04-13 10:37:19 +0900

    fix arrange migrate files fix #23 #6

--- plugins/calls/db/migrate/001_create_calls.rb
+++ plugins/calls/db/migrate/001_create_calls.rb
@@ -2,6 +2,7 @@ class CreateCalls < ActiveRecord::Migration
   def change
     create_table :calls do |t|
       t.integer :count, default: 0, null: false
+      t.integer :issue_id, null: false
       t.timestamps null: false
     end
   end
